0efd32762268fb0ee26ca80bd6e5abcaae79ad32
ParroT <parrotdance95@gmail.com>
2020-05-21 18:27:59 +0800

    update

--- script/parse.js
+++ script/parse.js
@@ -4,6 +4,9 @@ const { join } = require('path')
 const files = fs.readdirSync(join(__dirname, '..', 'src'))
 
 const readmePath = join(__dirname, '..', 'README.md')
-const content = files.reverse().reduce((content, file) => content + `- [${file.split('.')[0]}](./src/${file})`, `# 目录\n\n`)
+const header = `# 目录\n\n`
+const content = files
+  .reverse()
+  .reduce((content, file) => content + `- [${file.split('.')[0]}](./src/${file})\n`, header)
 
 fs.writeFileSync(readmePath, content, 'utf-8')
