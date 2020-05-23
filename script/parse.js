bc2a18e19987ab41c79c51e315a3e08bde8054f2
ParroT <parrotdance95@gmail.com>
2020-05-23 20:56:11 +0800

    update

--- script/parse.js
+++ script/parse.js
@@ -3,6 +3,7 @@ const { join } = require('path')
 
 const files = fs.readdirSync(join(__dirname, '..', 'src'))
 
+// generate README.md
 const readmePath = join(__dirname, '..', 'README.md')
 const header = `# 目录\n\n`
 const content = files
@@ -10,3 +11,17 @@ const content = files
   .reduce((content, file) => content + `- [${file.split('.')[0]}](./src/${file})\n`, header)
 
 fs.writeFileSync(readmePath, content, 'utf-8')
+
+// generate manifest.json
+const manifest = {
+  list: files.map(f => ({
+    id: f.split(' ')[0].replace('#', ''),
+    name: f
+  }))
+}
+
+const manifestPath = join(__dirname, '..', 'manifest.json')
+if (fs.existsSync(manifestPath)) {
+  fs.removeSync(manifestPath)
+}
+fs.writeFileSync(manifestPath, JSON.stringify(manifest))
