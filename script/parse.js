3669560029df40066164b3f6cdf9a72b5fed0af1
ParroT <parrotdance95@gmail.com>
2020-05-24 01:27:31 +0800

    update article names

--- script/parse.js
+++ script/parse.js
@@ -8,18 +8,9 @@ const readmePath = join(__dirname, '..', 'README.md')
 const header = `# 目录\n\n`
 const content = files
   .reverse()
-  .reduce((content, file) => content + `- [${file.split('.')[0]}](./src/${file})\n`, header)
+  .reduce((content, file) => 
+    content + `- [${file}](./src/${file})\n`,
+    header
+  )
 
 fs.writeFileSync(readmePath, content, 'utf-8')
-
-// generate manifest.json
-const manifest = {
-  list: files.map(f => ({
-    id: f.split(' ')[0].replace('#', ''),
-    name: f.replace('.md', '')
-  }))
-}
-
-const manifestPath = join(__dirname, '..', 'manifest.json')
-
-fs.writeFileSync(manifestPath, JSON.stringify(manifest))
